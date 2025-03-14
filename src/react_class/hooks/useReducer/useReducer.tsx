6a188ccfb54ba62d4d3de73eee720dba8a35a017
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2025-03-14 09:33:19 +0800

    added useReducer example

--- src/react_class/hooks/useReducer/useReducer.tsx
+++ src/react_class/hooks/useReducer/useReducer.tsx
@@ -1,122 +1,74 @@
 import { useReducer, useState } from "react";
 
 // // Define the reducer function
-// const counterReducer = (
-//   state: { count: number },
-//   action: { type: string; sample?: number }
-// ) => {
-//   switch (action.type) {
-//     case "increment":
-//       return { count: state.count + 1 };
-//     case "decrement":
-//       return { count: state.count - 1 };
-//     case "reset":
-//       return { count: (state.count = 0) };
-//     default:
-//       return state;
-//   }
-// };
-
-// export const Counter = () => {
-//   const [state, dispatch] = useReducer(counterReducer, { count: 0 });
-
-//   return (
-//     <div>
-//       <p>Count: {state.count}</p>
-//       <button onClick={() => dispatch({ type: "increment", sample: 200 })}>
-//         Increment
-//       </button>
-//       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
-//       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-//     </div>
-//   );
-// };
-
-interface TodoListInterface {
-  id: number;
-  task: string;
-  isDone: boolean;
-}
-
-const todoReducer = (
-  state: { toDoList: TodoListInterface[] },
-  action: { type: string; todo?: TodoListInterface }
+const counterReducer = (
+  state: { count: number },
+  action: { type: string; sample?: number }
 ) => {
   switch (action.type) {
-    case "ADD":
-      return { toDoList: [...state.toDoList, action.todo!] };
-    case "DONE":
-      let updateTodo: TodoListInterface[] = [];
-      state.toDoList.map((data) => {
-        if (data.id.toString() == action.todo?.id.toString()) {
-          updateTodo.push({ ...data, isDone: true });
-        } else {
-          updateTodo.push(data);
-        }
-      });
-      return { toDoList: updateTodo };
-    case "RESET":
-      return { toDoList: [] };
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: (state.count = 0) };
     default:
       return state;
   }
 };
 
-export const TodoReducer = () => {
-  const [state, dispatch] = useReducer(todoReducer, { toDoList: [] });
+export const Counter = () => {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
 
-  const [task, setTask] = useState<string>("");
   return (
-    <div style={{ display: "grid" }}>
-      <div style={{ display: "flex" }}>
-        <input type="text" onBlur={(data) => setTask(data.target.value)} />
-        <button
-          onClick={() =>
-            dispatch({
-              type: "RESET",
-            })
-          }
-        >
-          Reset
-        </button>
-        <button
-          onClick={() =>
-            dispatch({
-              type: "ADD",
-              todo: {
-                id: state.toDoList.length + 1,
-                task: task,
-                isDone: false,
-              },
-            })
-          }
-        >
-          Add Todo
-        </button>
-      </div>
-
-      {state.toDoList.map((data: TodoListInterface, index: number) => {
-        return (
-          <div style={{ display: "flex" }}>
-            <label
-              key={index}
-              style={{ color: data.isDone ? "gray" : "black" }}
-            >
-              {data.task}
-            </label>
-            <button
-              onClick={() =>
-                dispatch({
-                  type: "DONE",
-                  todo: data,
-                })
-              }
-            >
-              Mark as done
-            </button>
-          </div>
-        );
-      })}
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment", sample: 200 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
+  );
+};
+
+const themeReducer = (
+  state: { color: string; themeName: string },
+  action: { type: string }
+) => {
+  switch (action.type) {
+    case "GREEN":
+      return { color: "green", themeName: "Green" };
+    case "GRAY":
+      return { color: "gray", themeName: "Gray" };
+    case "BLUE":
+      return { color: "blue", themeName: "Blue" };
+    default:
+      return state;
+  }
+};
+
+export const ChangeTheme = () => {
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: "red",
+    themeName: "Red"
+  });
+
+  return (
+    <>
+      <button onClick={() => dispatch({ type: "GREEN" })}>GREEN</button>
+      <button onClick={() => dispatch({ type: "GRAY" })}>BLACK</button>
+      <button onClick={() => dispatch({ type: "BLUE" })}>BLUE</button>
+      <div
+        style={{
+          marginTop: 10,
+          height: "300px",
+          width: "300px",
+          backgroundColor: `${state.color}`
+        }}
+      >
+        Current theme is {state.themeName}
+      </div>
+    </>
   );
 };
--- src/views/containers/EmployeeList/EmployeeList.tsx
+++ src/views/containers/EmployeeList/EmployeeList.tsx
@@ -1,11 +1,6 @@
 import { Container } from "@mui/material";
 import React from "react";
-import { TodoReducer } from "../../../react_class/hooks/useReducer/useReducer";
 
 export const EmployeeList: React.FC<{}> = () => {
-  return (
-    <Container maxWidth="sm">
-      <TodoReducer />
-    </Container>
-  );
+  return <Container maxWidth="sm"></Container>;
 };
